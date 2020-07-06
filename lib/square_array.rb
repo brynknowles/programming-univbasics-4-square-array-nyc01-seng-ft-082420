8f1ecf70d0807b35ae567a7136deff409320f293
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-06 19:54:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
+# define a method called square_array
+# iterate over each element in the array using a while loop
+# square each element in the array
+# return each element in the array in a new array
+# use only while, arrays and other previously discussed methods for this solution
+
+def square_array(array, number)
   while
 end
